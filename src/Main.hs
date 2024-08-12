ca6782de2e08f3fd4ed4c123dee412b91fb32661
Gabriel <bragasupergod@gmail.com>
2024-08-12 15:11:32 -0700

    feat: setup the project architecture

--- src/Main.hs
+++ src/Main.hs
@@ -2,4 +2,70 @@ module Main (main) where
 
 main :: IO ()
 main = do
-  putStrLn "hello world"
+  putStrLn "Game Project"
+
+{-
+  Definicao de tipos de dados
+-}
+
+
+
+{-
+  Funcoes auxiliares, que nos irao ajudar a criar o jogo
+-}
+
+
+
+{-
+  Funcoes que criam o mapa do jogo
+-}
+
+
+
+{-
+  Funcoes que criam o boneco do jogo
+-}
+
+
+
+{-
+  Funcoes que capturam os eventos do teclado
+-}
+
+
+
+{-
+  Funcoes que fazem a movimentacao do boneco
+-}
+
+
+
+{-
+  Funcoes que criam os obstaculos do jogo
+-}
+
+
+
+{-
+  Funcoes que fazem a movimentacao dos obstaculos
+-}
+
+
+
+{-
+  Funcoes que fazem os obstaculos aparecerem/desaparecerem
+-}
+
+
+
+{-
+  Funcoes que fazem a colisao do boneco com os obstaculos
+-}
+
+
+
+{-
+  Funcoes que criam o estado inicial do jogo
+-}
+
+
